5f1098562d3992eca8d6c95cc9ebd13149b7048f
Fernando Toledo <fernando_29837@live.com>
2020-11-03 22:38:11 -0600

    Implementing Add Product component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 // import { ProductList } from './components/productManagemen/all-products-page/viewProductList/viewProductList';
 import AllProductsPage from './components/productManagemen/all-products-page/all-products-page';
 import { Register } from './components/authentication/registration/register/register';
+import { AddProduct } from './components/productManagemen/addProduct/addProduct';
 library.add(fas);
 
 function App() {
@@ -27,7 +28,8 @@ function App() {
 				<Route path="/login" component={SignIn} />
 				<Route path="/register" component={Register} />
 				<Route path="/products" component={AllProductsPage} />
-				{/* <Route path="/addProduct" component={AddProductPage} />
+				<Route path="/addProduct" component={AddProduct} />
+				{/* 
 				<Route path="/product/:productName" component={ProductDetail} /> */}
 			</Switch>
 		</Router>
---
+++ src/components/productManagemen/addProduct/addProduct.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export const AddProduct = () => {
+	return (
+		<div>
+
+			<h1> Add Product</h1>
+			
+		</div>
+	)
+}
