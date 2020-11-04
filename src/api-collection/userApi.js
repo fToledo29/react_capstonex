13b3eba4ac6300425aa59c09ea821ebc957938f2
Fernando Toledo <fernando_29837@live.com>
2020-11-03 22:26:09 -0600

    Adding HTTP Calls

--- src/api-collection/userApi.js
+++ src/api-collection/userApi.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 import uuid from 'uuidv4';
-export default class ProductsApi {
+export default class UserApi {
 
 	static saveProduct(product) {
 
 		product.id = uuid();
-		return axios.post('http://localhost:3004/products/', product).then(res => res.data);
+		return axios.post('http://localhost:3004/products/', product)
+		.then(res => res.data);
 	}
 
 	static getAllProducts() {
 		return axios.get('http://localhost:3004/products/').then(res => res.data);
+	}
+
+	static login(userName, pass) {
+		return axios.get(`http://localhost:3004/users?userName=${userName}&password=${pass}`)
+		.then(res => res.data);
 	}
 }
