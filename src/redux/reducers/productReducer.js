3f0193dff75495bffefb29a894ef5263a99e959a
Fernando Toledo <fernando_29837@live.com>
2020-12-03 22:48:03 -0600

    Product reducer updated

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -12,87 +12,72 @@ export default function productReducer(state = initialState, action) {
 	switch (action.type) {
 		case actionTypes.LOAD_PRODUCTS_SUCCESS:
 			return {
+				...state,
 				products: action.products,
 				productsCopy: [...action.products],
-				productToUpdate: {},
-				productsToDelete: [...state.productsToDelete],
-				productViewMode: false,
 			};
 		case actionTypes.ADD_PRODUCT: // TODO: Delete repeated code.
 			return {
+				...state,
 				products: [
 					...state.products, 
 					Object.assign({}, action.product)
 				],
-				productsCopy: [...state.products],
-				productToUpdate: {},
-				productsToDelete: [],
-				productViewMode: false,
 			};
 		case actionTypes.GET_PRODUCT:
 			return {
+				...state,
 				products: [
 					...state.products, 
 					Object.assign({}, action.product)
 				],
-				productsCopy: [...state.products],
-				productToUpdate: {},
-				productsToDelete: [],
-				productViewMode: false,
 			};
 		case actionTypes.PRODUCT_TO_UPDATE:
 			return {
-				products: [...state.products],
-				productsCopy: [],
+				...state,
 				productToUpdate: action.product,
-				productsToDelete: [],
-				productViewMode: false,
 			};
 		case actionTypes.PRODUCT_TO_DELETE:
 			return {
-				products: [...state.products],
-				productsCopy: [...state.products],
+				...state,
 				productToUpdate: [],
 				productsToDelete: [...state.productsToDelete, action.product],
 				productViewMode: false,
 			};
 		case actionTypes.REMOVE_PRODUCT_FROM_LIST_TO_DELETE:
 			return {
-				products: [...state.products],
-				productsCopy: [...state.products],
+				...state,
 				productToUpdate: [],
 				productsToDelete: [...action.productsToDelete],
 				productViewMode: false,
 			};
 		case actionTypes.CLEAR_PRODUCTS_TO_DELETE:
 			return {
-				products: [...state.products],
-				productsCopy: [...state.products],
+				...state,
 				productToUpdate: [],
 				productsToDelete: [],
 				productViewMode: false,
 			};
 		case actionTypes.DELETE_PRODUCT:
 			return {
+				...state,
 				products: [...action.products],
 				productsCopy: [...action.products],
 				productToUpdate: [],
-				productsToDelete: [...state.productsToDelete],
 				productViewMode: false,
 			};
 		case actionTypes.FILTER_PRODUCT:
 			const products = [...state.products];
 			const filter = action.productName ? new RegExp(action.productName) : null;
 			return {
-				products: [...state.products],
+				...state,
 				productsCopy: filter ? [...products.filter((product) => filter.test(product.productName))] : [...state.products],
 				productToUpdate: [],
-				productsToDelete: [...state.productsToDelete],
 				productViewMode: false,
 			};
 		case actionTypes.VIEW_PRODUCT:
 			return {
-				products: [...state.products],
+				...state,
 				productsCopy: [],
 				productToUpdate: [],
 				productsToDelete: [],
