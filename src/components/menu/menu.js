dea7c71e13ad452ac0410188e35ebafd0e509284
Fernando Toledo <fernando_29837@live.com>
2020-12-13 13:23:43 -0600

    Menu and All product page made responsive

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -24,48 +24,54 @@ const Menu = (props) => {
 
 	return (
 		<div>
-			<Navbar bg="dark" variant="dark">
-				<Nav className="mr-auto">
-					<NavLink className="nav-link" exact activeClassName="active" to="/">
-						<FontAwesomeIcon size="1x" className="icons" icon="home" />
-						<span className="login-label">Home</span>
-					</NavLink>
-					<NavLink className="nav-link menu-about" activeClassName="active" to="/about">About</NavLink>
-					<NavLink className="nav-link menu-products" activeClassName="active" to="/products">Products</NavLink>
-					<NavLink className="nav-link menu-chart" activeClassName="active" to="/chart">Top Viewed Products</NavLink>
-				</Nav>
-				
-				{props.userData.loggedIn && 
-				!!props.userData.user &&
-				!!props.userData.user.userName ? <Form className="nav-buttons nav-user" inline>
-					<Dropdown>
-						<Dropdown.Toggle variant="success" id="dropdown-basic">
-							{props.userData.user.userName}
-						</Dropdown.Toggle>
+			<Navbar animation="false" bg="dark" expand="lg" variant="dark">
 
-						<Dropdown.Menu>
-							<Dropdown.Item className="loggedin-user-profile" onClick={() => history.push('/userDetails')}>Profile</Dropdown.Item>
-							<Dropdown.Item className="loggedin-user-logout" onClick={() => logout()}>Logout</Dropdown.Item>
-						</Dropdown.Menu>
-					</Dropdown>
-				</Form> :
-				<Form className="nav-buttons" inline>
+				<Navbar.Toggle animation="false" aria-controls="basic-navbar-nav" />	
+				<Navbar.Collapse animation="false" id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<NavLink className="nav-link" exact activeClassName="active" to="/">
+							<FontAwesomeIcon size="1x" className="icons" icon="home" />
+							<span className="login-label">Home</span>
+						</NavLink>
+						<NavLink className="nav-link menu-about" activeClassName="active" to="/about">About</NavLink>
+						<NavLink className="nav-link menu-products" activeClassName="active" to="/products">Products</NavLink>
+						<NavLink className="nav-link menu-chart" activeClassName="active" to="/chart">Top Viewed Products</NavLink>
+					</Nav>
+
+					{props.userData.loggedIn && 
+					!!props.userData.user &&
+					!!props.userData.user.userName ? <Form className="nav-buttons nav-user" inline>
+						<Dropdown>
+							<Dropdown.Toggle variant="success" id="dropdown-basic">
+								{props.userData.user.userName}
+							</Dropdown.Toggle>
+
+							<Dropdown.Menu>
+								<Dropdown.Item className="loggedin-user-profile" onClick={() => history.push('/userDetails')}>Profile</Dropdown.Item>
+								<Dropdown.Item className="loggedin-user-logout" onClick={() => logout()}>Logout</Dropdown.Item>
+							</Dropdown.Menu>
+						</Dropdown>
+					</Form> :
+					<Form className="nav-buttons" inline>
+							
+							<Button variant="outline-info">
+								<NavLink activeClassName="active" to="/login">
+									<FontAwesomeIcon size="1x" className="icons" icon={"sign-in-alt"} />	
+									<span className="login-label login">Login</span>
+								</NavLink>
+							</Button>
+
+							<Button variant="outline-info">
+								<NavLink activeClassName="active" to="/register">
+									<FontAwesomeIcon size="1x" className="icons" icon={"user-plus"} />	
+									<span className="login-label register">Register</span>
+								</NavLink>
+							</Button>
+
+						</Form>}
 					
-					<Button variant="outline-info">
-						<NavLink activeClassName="active" to="/login">
-							<FontAwesomeIcon size="1x" className="icons" icon={"sign-in-alt"} />	
-							<span className="login-label login">Login</span>
-						</NavLink>
-					</Button>
-
-					<Button variant="outline-info">
-						<NavLink activeClassName="active" to="/register">
-							<FontAwesomeIcon size="1x" className="icons" icon={"user-plus"} />	
-							<span className="login-label register">Register</span>
-						</NavLink>
-					</Button>
-
-				</Form>}
+					
+				</Navbar.Collapse>
 			</Navbar>
 		</div>
 	)
